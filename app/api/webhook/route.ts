ff06f991d43329cb6c1a32e6eaeea98b428710d2
Elijah Gummer <elijahgummer5@gmail.com>
2025-06-26 10:25:26 +1000

    added the logo

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe"
 import { sendOrderConfirmationEmail } from "@/lib/email"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2025-05-28.basil",
 })
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!
